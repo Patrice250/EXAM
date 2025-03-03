0230c09c3718e2f491b1fc0a763301bd45dc97a1
Patrice250 <patricetuyizere1@gmail.com>
2025-03-03 16:12:10 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RP Ngoma College</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <a href="index2.html">Home</a>
+        <a href="gallery.php">Gallery</a>
+        <a href="about.php">About Us</a>
+    </nav>
