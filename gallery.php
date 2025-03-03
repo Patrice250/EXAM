4776ae39acd3583f512c80bb1850c923f953f2bc
Patrice250 <patricetuyizere1@gmail.com>
2025-03-03 16:13:29 +0200

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+    <h1>Gallery</h1>
+    <div class="gallery">
+        <img src="images/im1.jpg" alt="College Image 1">
+        <img src="images/im2.jpg" alt="College Image 2">
+        <img src="images/im3.jpg" alt="College Image 3">
+    </div>
+<?php include 'footer.php'; ?>
